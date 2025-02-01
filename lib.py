8a5fd2816cbc7d554bb82c402f5ff721c1bab2af
WilliamBak6 <114810020+WilliamBak6@users.noreply.github.com>
2025-02-01 17:25:19 +0100

    google env

--- lib.py
+++ lib.py
@@ -7,7 +7,9 @@ liste = [
     "json5", 
     "python-dotenv",
     "google-cloud",
-    "google-cloud-bigquery-connection"
+    "google-cloud-bigquery-connection",
+    "google-cloud-storage",
+    "google-api-python-client"
 ] 
 
 for i in liste :
